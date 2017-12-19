42f100bcbc7c9dd43a249bc70859a0eba712ead0
Vibhanshu Vaibhav <vibhanshu_chaudhary@yahoo.co.in>
2017-12-19 13:50:54 +0530

    Initial commit

---
+++ Dev Profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dev Profile
+//
+//  Created by Vibhanshu Vaibhav on 22/08/17.
+//  Copyright © 2017 Vibhanshu Vaibhav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
